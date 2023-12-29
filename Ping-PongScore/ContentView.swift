a58ab17c82485b38ce890bae9eb0b384ad3293fb
Kim Eunji <qoopej@gmail.com>
2023-12-29 15:43:47 +0900

    Score+

--- Ping-PongScore/ContentView.swift
+++ Ping-PongScore/ContentView.swift
@@ -8,30 +8,71 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    
+    @State var userOneText = 0
+    @State var userTwoText = 0
+    
     var body: some View {
+        
         VStack {
             
             ZStack {
-                // 사각형
+                // 첫번째 유저 사각형
                 Rectangle()
                     .frame(width: 130, height: 130)
                     .foregroundColor(.red)
                     .offset(x: -80, y: -160)
+                    .onTapGesture {
+                        userOneScore()
+                    }
+                Text("\(userOneText)")
+                    .foregroundStyle(.yellow)
+                    .bold()
+                    .font(.largeTitle)
+                    .offset(x: -80, y: -160)
+                
                 
                 Rectangle()
                     .frame(width: 130, height: 130)
                     .foregroundColor(.red)
                     .offset(x: 80, y: -160)
+                    .onTapGesture {
+                        userTwoScore()
+                    }
+                
+                Text("\(userTwoText)")
+                    .foregroundStyle(.yellow)
+                    .bold()
+                    .font(.largeTitle)
+                    .offset(x: 80, y: -160)
             }
             
+            
         }
-    
+        
         
         
         .padding()
+        
+        
     }
+    
+    fileprivate func userOneScore() {
+        print(#fileID, #function, #line, "- <# 주석 #>")
+        userOneText += 1
+    }
+    
+    fileprivate func userTwoScore() {
+        print(#fileID, #function, #line, "- <# 주석 #>")
+        userTwoText += 1
+    }
+    
+    
 }
 
 #Preview {
     ContentView()
 }
+
+
