3903c167049572164391f740c3938de819bdccee
Kim Eunji <qoopej@gmail.com>
2024-01-01 18:52:27 +0900

    SetScore logging

--- Ping-PongScore/ContentView.swift
+++ Ping-PongScore/ContentView.swift
@@ -30,8 +30,16 @@ struct LandscapeOnlyViewControllerWrapper: UIViewControllerRepresentable {
 struct ContentView: View {
     
     
-    @State private var userOneText = 0
-    @State private var userTwoText = 0
+    @State private var userOneText = 0 {
+        didSet {
+            winScore()
+        }
+    }
+    @State private var userTwoText = 0 {
+        didSet {
+            winScore()
+        }
+    }
     @State private var flipDegreesUserOne = 0.0
     @State private var flipDegreesUserTwo = 0.0
     @State private var flipPersDegreesUserOne = 0.0
@@ -42,7 +50,13 @@ struct ContentView: View {
             updateServing()
         }
     }
+    
     @State private var isUserOneServing = true  // 플레이어 원이 서브권을 가질 때
+    @State private var setScoreUserOne = 0
+    @State private var setScoreUserTwo = 0
+    @State private var deuce = false
+    @State private var gameOver = false
+    
     
     var body: some View {
         
@@ -61,40 +75,54 @@ struct ContentView: View {
                     )
                     .frame(width: 180, height: 250)
                     .foregroundColor(.red)
-                    .offset(x: -250, y: 0)
+                    .offset(x: -250, y: 80)
                     .rotation3DEffect(Angle(degrees: flipDegreesUserOne), axis: (x: 1, y: 0, z: 0), perspective: flipPersDegreesUserOne) // 뒤로 넘어가는 효과
                     .onTapGesture {
                         
-                        AddUserOneScore()
+                        addUserOneScore()
                     }
-                    .gesture(DragGesture(minimumDistance: 20, coordinateSpace: .local)
-                                        .onEnded({ value in
-                                            if value.translation.width < 0 {
-                                                // left
-                                            }
-
-                                            if value.translation.width > 0 {
-                                                // right
-                                            }
-                                            if value.translation.height < 0 {
-                                             AddUserOneScore()
-                                            }
-
-                                            if value.translation.height > 0 {
-                                                // down
-                                                subtractUserOneScore()
-                                            }
-                                        }))
+                    .gesture(DragGesture(minimumDistance: 0, coordinateSpace: .local)
+                        .onEnded({ value in
+                            if abs(value.translation.width) > abs(value.translation.height) {
+                                // 수평 이동이 더 클 경우
+                                if value.translation.width < 0 {
+                                    // 왼쪽으로 드래그
+                                    subtractUserOneScore()
+                                } else {
+                                    // 오른쪽으로 드래그
+                                    addUserOneScore()
+                                }
+                            } else {
+                                // 수직 이동이 더 클 경우
+                                if value.translation.height < 0 {
+                                    // 위로 드래그
+                                    addUserOneScore()
+                                } else {
+                                    // 아래로 드래그
+                                    subtractUserOneScore()
+                                }
+                            }
+                        }))
                 Text("\(userOneText)")
                     .foregroundStyle(.yellow)
                     .bold()
                     .font(.largeTitle)
-                    .offset(x: -250, y: 0)
+                    .offset(x: -250, y: 80)
                 // 서브권
                 Rectangle()
                     .frame(width: 160, height: 10)
-                    .offset(x: -250, y: 110)
+                    .offset(x: -250, y: 190)
                     .foregroundColor(isUserOneServing ? .indigo : .clear)
+                // 세트 스코어
+                Rectangle()
+                    .frame(width:70, height: 90)
+                    .foregroundColor(.red)
+                    .offset(x: -110, y: 160)
+                Text("\(setScoreUserOne)")
+                    .foregroundStyle(.yellow)
+                    .bold()
+                    .font(.largeTitle)
+                    .offset(x: -110, y: 160)
                 
                 
                 
@@ -110,69 +138,105 @@ struct ContentView: View {
                     )
                     .frame(width: 180, height: 250)
                     .foregroundColor(.red)
-                    .offset(x: 250, y: 0)
+                    .offset(x: 250, y: 80)
                     .rotation3DEffect(Angle(degrees: flipDegreesUserTwo), axis: (x: 1, y: 0, z: 0), perspective: flipPersDegreesUserTwo) // 뒤로 넘어가는 효과
                     .onTapGesture {
-                        AddUserTwoScore()
+                        addUserTwoScore()
                     }
-                    .gesture(DragGesture(minimumDistance: 20, coordinateSpace: .local)
-                                        .onEnded({ value in
-                                            if value.translation.width < 0 {
-                                                // left
-                                            }
-
-                                            if value.translation.width > 0 {
-                                                // right
-                                            }
-                                            if value.translation.height < 0 {
-                                            AddUserTwoScore()
-                                            }
-
-                                            if value.translation.height > 0 {
-                                               subtractUserTwoScore()
-                                            }
-                                        }))
+                    .gesture(DragGesture(minimumDistance: 0, coordinateSpace: .local)
+                        .onEnded({ value in
+                            if abs(value.translation.width) > abs(value.translation.height) {
+                                // 수평 이동이 더 클 경우
+                                if value.translation.width < 0 {
+                                    // 왼쪽으로 드래그
+                                    subtractUserTwoScore()
+                                } else {
+                                    // 오른쪽으로 드래그
+                                    addUserTwoScore()
+                                }
+                            } else {
+                                // 수직 이동이 더 클 경우
+                                if value.translation.height < 0 {
+                                    // 위로 드래그
+                                    addUserTwoScore()
+                                } else {
+                                    // 아래로 드래그
+                                    subtractUserTwoScore()
+                                }
+                            }
+                        }))
                 // 서브권
                 Rectangle()
                     .frame(width: 160, height: 10)
-                    .offset(x: 250, y: 110)
+                    .offset(x: 250, y: 190)
                     .foregroundColor(isUserOneServing ? .clear : .indigo)
                 
                 Text("\(userTwoText)")
                     .foregroundStyle(.yellow)
                     .bold()
                     .font(.largeTitle)
-                    .offset(x: 250, y: 0)
+                    .offset(x: 250, y: 80)
             }
             
+            // 세트 스코어
+            Rectangle()
+                .frame(width:70, height: 90)
+                .foregroundColor(.red)
+                .offset(x: 110, y: -30)
+            Text("\(setScoreUserTwo)")
+                .foregroundStyle(.yellow)
+                .bold()
+                .font(.largeTitle)
+                .offset(x: 110, y: -190)
             
         }
-        
-        
-        
         .padding()
         .background(LandscapeOnlyViewControllerWrapper())
         
     }
     
-    fileprivate func AddUserOneScore() {
-        print(#fileID, #function, #line, "- <# 주석 #>")
-        withAnimation(.easeInOut(duration: 0.5)) {
-            allScore += 1
-            userOneText += 1
+    fileprivate func addUserOneScore() {
+        allScore += 1
+        userOneText += 1
+
+        withAnimation(.easeInOut(duration: 0.25)) {
+            // Rotate halfway
             flipDegreesUserOne += 180
-            flipPersDegreesUserOne = 0.5
+        }
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+            withAnimation(.easeInOut(duration: 0.1)) {
+                // Complete the flip
+                flipDegreesUserOne += 180
+
+                // Reset to 0 (equivalent to 360) for next flip
+                if flipDegreesUserOne >= 360 {
+                    flipDegreesUserOne = 0
+                }
+            }
         }
     }
     
     
-    fileprivate func AddUserTwoScore() {
-        print(#fileID, #function, #line, "- <# 주석 #>")
-        withAnimation(.easeInOut(duration: 0.5)) {
-            allScore += 1
-            userTwoText += 1
+    fileprivate func addUserTwoScore() {
+        allScore += 1
+        userTwoText += 1
+
+        withAnimation(.easeInOut(duration: 0.25)) {
+            // Rotate halfway
             flipDegreesUserTwo += 180
-            flipPersDegreesUserTwo = 0.5
+        }
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+            withAnimation(.easeInOut(duration: 0.1)) {
+                // Complete the flip
+                flipDegreesUserTwo += 180
+
+                // Reset to 0 (equivalent to 360) for next flip
+                if flipDegreesUserTwo >= 360 {
+                    flipDegreesUserTwo = 0
+                }
+            }
         }
     }
     
@@ -183,11 +247,24 @@ struct ContentView: View {
             return
         }
         
-        withAnimation(.easeInOut(duration: 0.5)) {
-            allScore -= 1
-            userOneText -= 1
+        allScore -= 1
+        userOneText -= 1
+
+        withAnimation(.easeInOut(duration: 0.25)) {
+            // Rotate halfway in the reverse direction
             flipDegreesUserOne -= 180
-            flipPersDegreesUserOne = -0.5
+        }
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+            withAnimation(.easeInOut(duration: 0.1)) {
+                // Complete the reverse flip
+                flipDegreesUserOne -= 180
+
+                // Reset to 0 (equivalent to -360) for next flip
+                if flipDegreesUserOne <= -360 {
+                    flipDegreesUserOne = 0
+                }
+            }
         }
     }
     
@@ -198,22 +275,89 @@ struct ContentView: View {
             return
         }
         
-        withAnimation(.easeInOut(duration: 0.5)) {
-            allScore -= 1
-            userTwoText -= 1
+        allScore -= 1
+        userTwoText -= 1
+
+        withAnimation(.easeInOut(duration: 0.25)) {
+            // Rotate halfway in the reverse direction
             flipDegreesUserTwo -= 180
-            flipPersDegreesUserTwo = -0.5
+        }
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+            withAnimation(.easeInOut(duration: 0.1)) {
+                // Complete the reverse flip
+                flipDegreesUserTwo -= 180
+
+                // Reset to 0 (equivalent to -360) for next flip
+                if flipDegreesUserTwo <= -360 {
+                    flipDegreesUserTwo = 0
+                }
+            }
         }
     }
     
     fileprivate func updateServing() {
-        if allScore % 4 == 0 {
+        if allScore % 2 == 0 {
             isUserOneServing.toggle()
         }
     }
     
+    fileprivate func winScore() {
+        
+        if gameOver { return }
+        
+        
+        // 듀스게임
+        if deuce {
+            // abs는 절대값 함수
+            if abs(userOneText - userTwoText) == 2 {
+                gameOver = true
+                // Deuce resolution
+                if userOneText > userTwoText {
+                    setScoreUserOne += 1
+                    print(#fileID, #function, #line, "- 듀스에서 userOne이 이겼습니다.")
+                } else {
+                    setScoreUserTwo += 1
+                    print(#fileID, #function, #line, "- 듀스에서 userTwo가 이겼습니다.")
+                }
+                resetGame()
+            }
+        } else {
+            // Normal play
+            if (userOneText >= 11 || userTwoText >= 11) && abs(userOneText - userTwoText) >= 2 {
+                gameOver = true
+                if userOneText > userTwoText {
+                    setScoreUserOne += 1
+                    print(#fileID, #function, #line, "- userOne이 이겼습니다!")
+                } else {
+                    setScoreUserTwo += 1
+                    print(#fileID, #function, #line, "- userTwo가 이겼습니다.")
+                }
+                resetGame()
+            } else if userOneText == 10 && userTwoText == 10 {
+                // 듀스입니다!
+                deuce = true
+                print(#fileID, #function, #line, "- 듀스입니다.")
+            }
+        }
+    }
+    
+    fileprivate func resetGame() {
+        userOneText = 0
+        userTwoText = 0
+        allScore = 0
+        deuce = false
+        gameOver = false
+    }
+
+    
     
 }
+
+
+
+// 화면이 안 꺼지게 하기 앱이 Foreground에 있을 떄
+// 서브 권이 맨 처음 언제인지 표시하기!
 
 #Preview {
     ContentView()
